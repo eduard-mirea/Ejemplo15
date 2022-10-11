7c5ca9bc701cb609e469e832afb2e52e50ea35e2
eduard-mirea <alumir79@ieselcaminas.org>
2022-10-11 20:51:56 +0200

    Initial commit

---
+++ src/Ejercicio15.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Ejercicio15 {
+    public static void main(String[] args) {
+        int nota;
+
+        Scanner inputValue = new Scanner(System.in);
+        System.out.println("Introduce la nota del examen: ");
+
+        nota = inputValue.nextInt();
+
+        if ((nota<0) || (nota>10)){
+            System.out.println("Nota invalida");
+        }else if (nota<3){
+            System.out.println("Muy deficiente");
+        }else if (nota<5){
+            System.out.println("Insuficiente");
+        }else if (nota<6){
+            System.out.println("Suficiente");
+        }else if (nota<7){
+            System.out.println("Bien");
+        }else if (nota<9){
+            System.out.println("Notable");
+        }else if (nota<10){
+            System.out.println("Sobresaliente");
+        }
+    }
+}
